1c3c3add4e452e895374df390a42039734795177
sarkarjeet8013-gif <sarkarjeet8013@gmail.com>
2025-12-29 19:31:10 +0530

    Update_main.py

--- Update_main.py
+++ Update_main.py
@@ -4,4 +4,4 @@ st.title("Interactive Streamlit App")
 
 name=st.type_title("Enter your Name: ")
 if st.button("Submit"):
-print("Hello!", name, "Welcome to Streamlit")
+  st.write("Hello!", name, "Welcome to Streamlit")
